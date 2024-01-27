001efa92d3340bf10fcbd09096d52d2ea373382a
Srinath Kulkarni <talk2sri@gmail.com>
2024-01-26 21:40:42 -0500

    Added few controller and configuration files

--- src/main/java/com/mylearnsb/springboot/mylearnspringboot/CaptureData.java
+++ src/main/java/com/mylearnsb/springboot/mylearnspringboot/CaptureData.java
@@ -13,7 +13,8 @@ public class CaptureData {
 	@RequestMapping("/capture")
 	public List<CaptureDataSource> retrieveAllData(){
 		return Arrays.asList( new CaptureDataSource(1,"Pega Infinity","BPM", "Pega tool helps the smothen the business process with proper defined work flows"),
-				new CaptureDataSource(2,"Guidewire Claimcenter", "Workflows", "Claimcenter tool helps to enhance working process for Insurance claim industry")
+				new CaptureDataSource(2,"Guidewire Claimcenter", "Workflows", "Claimcenter tool helps to enhance working process for Insurance claim industry"),
+				new CaptureDataSource(3,"iLog", "Workflows", "iLog is a workflow building tool")
 				);
 	}
 
